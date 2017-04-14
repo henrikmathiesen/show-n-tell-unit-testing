5cd83b7723451436cd36b29d34fc10fccf6930e9
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-04-14 09:04:31 +0200

    -installed packages for angular -project structure

---
+++ test-with-angular/gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+
+var config = {
+    src: {
+        app: ['./src/**/*.module.js', './src/**/*.js']
+    },
+    bld: './bld',
+    server: {
+        port: 1337
+    }
+};
