98c7e9e919e1936dca07cfb94e80ed549cbff0ce
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-04-15 07:10:38 +0200

    -testing

--- test-with-angular/src/gallery/gallery.directive.test.js
+++ test-with-angular/src/gallery/gallery.directive.test.js
@@ -1,3 +1,5 @@
+// describe blocks groups tests, can nest them
+
 describe('The Gallery Component should show images and their caption', function () {
 
     var $scope;
@@ -40,19 +42,52 @@ describe('The Gallery Component should show images and their caption', function 
             expect(jQelement.find('button').length).toBe(2);
         });
 
+        // it blocks run in random order, function scope apply
+
         it('should have the clickable buttons, for user to change to prev or next image', function () {
             expect(jQelement.find('button').eq(0).attr('ng-click')).toContain('vm.prev()');
             expect(jQelement.find('button').eq(1).attr('ng-click')).toContain('vm.next()');
         });
 
-        it('should have the image clickable, for user to get the caption', function(){
+        it('should have the image clickable, for user to get the caption', function () {
             expect(jQelement.find('img').attr('ng-click')).toContain('vm.getInfo()');
         });
 
-        it('should have a place to show the image caption; id and info', function(){
+        it('should have a place to show the image caption; id and info', function () {
             expect(jQelement.find('div[ng-show="vm.images[vm.index].info"]').length).toBe(1);
         });
+    });
 
+    describe('the functionality', function () {
+        var vm;
+
+        beforeEach(function () {
+            vm = $controller('galleryController');
+
+            // We can mock different images if we want
+            // var isolateScope = jQelement.isolateScope();
+            // isolateScope.vm.images = [
+            //     {
+            //         id: 1,
+            //         src: 'mock-1.png'
+            //     },
+            //     {
+            //         id: 2,
+            //         src: 'mock-2.png'
+            //     },
+            //     {
+            //         id: 3,
+            //         src: 'mock-3.png'
+            //     }
+            // ];
+
+            // $scope.$apply();
+        });
+
+        it('starts with the first image', function () {
+            expect(vm.index).toBe(0);
+            expect(jQelement.find('img').attr('src')).toContain('Untitled-1.png');
+        });
     });
 
 
