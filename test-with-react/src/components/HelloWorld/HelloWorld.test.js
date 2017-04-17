6f035ad9c044abe042d6dfac64444e3c58b3b214
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-04-17 10:14:40 +0200

    -spelling error

--- test-with-react/src/components/HelloWorld/HelloWorld.test.js
+++ test-with-react/src/components/HelloWorld/HelloWorld.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import HelloWorld from './HelloWorld';
 
-// Jest test api is very similiral to Jasmine
+// Jest test api is very similar to Jasmine
 
 describe("HelloWorld Component should greet the world in two languages", () => {
 
