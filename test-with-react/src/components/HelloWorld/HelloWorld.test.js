10cba67a2fd2832c074291bf7ba36800ec2dc300
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-04-17 10:14:26 +0200

    -comment

--- test-with-react/src/components/HelloWorld/HelloWorld.test.js
+++ test-with-react/src/components/HelloWorld/HelloWorld.test.js
@@ -3,6 +3,8 @@ import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import HelloWorld from './HelloWorld';
 
+// Jest test api is very similiral to Jasmine
+
 describe("HelloWorld Component should greet the world in two languages", () => {
 
     const component = shallow(
